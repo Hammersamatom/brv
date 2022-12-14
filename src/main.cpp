c7f4e90afce6e1e3bb78f3ffdec094a509dad31d
Hammersamatom <hammersamatom@gmail.com>
2022-12-14 16:01:32 -0500

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <cstdint>
 #include <fmt/core.h>
 
+
 union instr {
     uint32_t instruction;
+    struct __attribute__((__packed__)) op_only {
+        uint8_t opcode : 7;
+        uint32_t unused : 25;
+    } op_only;
     struct __attribute__((__packed__)) r_type {
         uint8_t opcode : 7;
         uint8_t rd : 5;
@@ -53,20 +58,279 @@ union instr {
 
 constexpr size_t tech = sizeof(instr);
 
+void unimplemented_instr(instr unimp)
+{
+    fmt::print("! UNIMPLEMENTED INSTRUCTION !\n");
+    fmt::print("HEX: {:08x}\n", unimp.instruction);
+    fmt::print("> Opcode: {:07b}\n", (uint8_t)unimp.op_only.opcode);
+}
+
+
 int main()
 {
     instr test;
-    test.instruction = 0x001101b3;
 
-    fmt::print("opcode in binary: {:07b}\n", (uint8_t)test.r_type.opcode);
-    fmt::print("rd in binary, and num: {:05b}, {:d}\n", (uint8_t)test.r_type.rd, (uint8_t)test.r_type.rd);
-    fmt::print("funct3 in binary: {:03b}\n", (uint8_t)test.r_type.funct3);
-    fmt::print("rs1 in binary, and num: {:05b}, {:d}\n", (uint8_t)test.r_type.rs1, (uint8_t)test.r_type.rs1);
-    fmt::print("rs2 in binary, and num: {:05b}, {:d}\n", (uint8_t)test.r_type.rs2, (uint8_t)test.r_type.rs2);
-    fmt::print("funct7 in binary: {:07b}\n", (uint8_t)test.r_type.funct7);
+    uint32_t rom_code[] = {
+        0x06408093, // ADDI x1, x1, 100
+        0x0c810113, // ADDI x2, x2, 200
+        0x001101b3  // ADD  x3, x2, x1
+    };
 
-    // What I'd expect
-    // 0000000 00001 00010 000 00011 0110011
+    uint32_t gp_regs[32];
+    uint32_t pc_reg = 0;
+
+    for (uint32_t i = 0; i < 1; i++)
+    {
+
+        //test.instruction = (rom_code[pc_reg + 3] << 24) | (rom_code[pc_reg + 2] << 16) | (rom_code[pc_reg + 1] << 8) | (rom_code[pc_reg]);
+        test.instruction = rom_code[i];//pc_reg];
+        switch (test.op_only.opcode)
+        {
+            case 0b0110011: // Integer ALU R-Type
+                switch (test.r_type.funct3)
+                {
+                    case 0x0: // ADD / SUB
+                        switch (test.r_type.funct7)
+                        {
+                            case 0x00:
+                                fmt::print("ADD instr\n");
+                                if (test.r_type.rd != 0) // Prevent write to x0 (AKA, hardwire 0)
+                                {
+                                    gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] + gp_regs[test.r_type.rs2];
+                                }
+                                //pc_reg+=1;
+                                break;
+                            case 0x20:
+                                fmt::print("SUB instr\n");
+                                if (test.r_type.rd != 0) // Prevent write to x0 (AKA, hardwire 0)
+                                {
+                                    gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] - gp_regs[test.r_type.rs2];
+                                }
+                                //pc_reg+=1;
+                                break;
+                            default:
+                                unimplemented_instr(test);
+                                //pc_reg+=1;
+                                break;
+                        }
+                        break;
+                    case 0x4: // XOR (RV32I only has one instruction for funct3)
+                        switch (test.r_type.funct7)
+                        {
+                            case 0x00:
+                                fmt::print("XOR instr\n");
+                                if (test.r_type.rd != 0) // Prevent write to x0 (AKA, hardwire 0)
+                                {
+                                    gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] ^ gp_regs[test.r_type.rs2];
+                                }
+                                //pc_reg+=1;
+                                break;
+                            default:
+                                unimplemented_instr(test);
+                                //pc_reg+=1;
+                                break;
+                        }
+                        break;
+                    case 0x6: // OR (RV32I only has one instruction for funct3)
+                        switch (test.r_type.funct7)
+                        {
+                            case 0x00:
+                                fmt::print("OR instr\n");
+                                if (test.r_type.rd != 0) // Prevent write to x0 (AKA, hardwire 0)
+                                {
+                                    gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] | gp_regs[test.r_type.rs2];
+                                }
+                                //pc_reg+=1;
+                                break;
+                            default:
+                                unimplemented_instr(test);
+                                //pc_reg+=1;
+                                break;
+                        }
+                        break;
+                    case 0x7: // AND (RV32I only has one instruction for funct3)
+                        switch (test.r_type.funct7)
+                        {
+                            case 0x00:
+                                fmt::print("AND instr\n");
+                                if (test.r_type.rd != 0) // Prevent write to x0 (AKA, hardwire 0)
+                                {
+                                    gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] & gp_regs[test.r_type.rs2];
+                                }
+                                //pc_reg+=1;
+                                break;
+                            default:
+                                unimplemented_instr(test);
+                                //pc_reg+=1;
+                                break;
+                        }
+                        break;
+                    case 0x1: // SLL (RV32I only has one instruction for funct3)
+                        switch (test.r_type.funct7)
+                        {
+                            case 0x00:
+                                fmt::print("SLL instr\n");
+                                if (test.r_type.rd != 0) // Prevent write to x0 (AKA, hardwire 0)
+                                {
+                                    gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] << gp_regs[test.r_type.rs2];
+                                }
+                                //pc_reg+=1;
+                                break;
+                            default:
+                                unimplemented_instr(test);
+                                //pc_reg+=1;
+                                break;
+                        }
+                        break;
+                    case 0x5: // SRL / SRA (Should be working fine)
+                        switch (test.r_type.funct7)
+                        {
+                            case 0x00:
+                                fmt::print("SRL instr\n");
+                                if (test.r_type.rd != 0) // Prevent write to x0 (AKA, hardwire 0)
+                                {
+                                    gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] >> gp_regs[test.r_type.rs2];
+                                }
+                                //pc_reg+=1;
+                                break;
+                            case 0x20:
+                                fmt::print("SRA instr\n");
+                                if (test.r_type.rd != 0) // Prevent write to x0 (AKA, hardwire 0)
+                                {
+                                    gp_regs[test.r_type.rd] = (int32_t)gp_regs[test.r_type.rs1] >> gp_regs[test.r_type.rs2];
+                                }
+                                //pc_reg+=1;
+                                break;
+                            default:
+                                unimplemented_instr(test);
+                                //pc_reg+=1;
+                                break;
+                        }
+                        break;
+                    case 0x2: // SLT (Set Less Than [signed?]) (RV32I only has one instruction for funct3)
+                        switch (test.r_type.funct7)
+                        {
+                            case 0x00:
+                                fmt::print("SLT instr\n");
+                                if (test.r_type.rd != 0) // Prevent write to x0 (AKA, hardwire 0)
+                                {
+                                    gp_regs[test.r_type.rd] = (signed)gp_regs[test.r_type.rs1] < (signed)gp_regs[test.r_type.rs2] ? 1 : 0;
+                                }
+                                //pc_reg+=1;
+                                break;
+                            default:
+                                unimplemented_instr(test);
+                                //pc_reg+=1;
+                                break;
+                        }
+                        break;
+                    case 0x3: // SLTU (Set Less Than Unsigned) (RV32I only has one instruction for funct3)
+                        switch (test.r_type.funct7)
+                        {
+                            case 0x00:
+                                fmt::print("SLTU instr\n");
+                                if (test.r_type.rd != 0) // Prevent write to x0 (AKA, hardwire 0)
+                                {
+                                    gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] < gp_regs[test.r_type.rs2] ? 1 : 0;
+                                }
+                                //pc_reg+=1;
+                                break;
+                            default:
+                                unimplemented_instr(test);
+                                //pc_reg+=1;
+                                break;
+                        }
+                        break;
+                    default:
+                        unimplemented_instr(test);
+                        break;
+                }
+                break;
+            case 0b0010011: // Integer ALU I-Type ////////// NOTE: Immediates are sign-extended (they're signed by DEFAULT, their MSB gets extended to the left [all ones to the left])
+                switch (test.i_type.funct3)
+                {
+                    case 0x0: // ADDI
+                        fmt::print("ADDI Instr\n");
+                        if (test.i_type.rd != 0)
+                        {
+                            gp_regs[test.i_type.rd] = gp_regs[test.i_type.rs1] + (test.i_type.imm >> 20);
+                        }
+                        break;
+                    case 0x4: // XORI
+                        fmt::print("XORI Instr\n");
+                        if (test.i_type.rd != 0)
+                        {
+                            gp_regs[test.i_type.rd] = gp_regs[test.i_type.rs1] ^ (test.i_type.imm >> 20);
+                        }
+                        break;
+                    case 0x6: // ORI
+                        fmt::print("ORI Instr\n");
+                        if (test.i_type.rd != 0)
+                        {
+                            gp_regs[test.i_type.rd] = gp_regs[test.i_type.rs1] | (test.i_type.imm >> 20);
+                        }
+                        break;
+                    case 0x7: // ANDI
+                        fmt::print("ANDI Instr\n");
+                        if (test.i_type.rd != 0)
+                        {
+                            gp_regs[test.i_type.rd] = gp_regs[test.i_type.rs1] & (test.i_type.imm >> 20);
+                        }
+                        break;
+                    case 0x1: // SLLI -- INCOMPLETE
+                        fmt::print("SLLI Instr\n");
+                        if (test.i_type.rd != 0)
+                        {
+                            gp_regs[test.i_type.rd] = gp_regs[test.i_type.rs1] << ((test.i_type.imm >> 20) & 0b11111);
+                        }
+                        break;
+                    case 0x5:
+                        switch ((test.i_type.imm & 0b111111100000) >> 5) // Subdivide the IMM value again with a union?
+                        {
+                            case 0x00:
+                                fmt::print("SRLI instr\n");
+                                if (test.i_type.rd != 0) // Prevent write to x0 (AKA, hardwire 0)
+                                {
+                                    gp_regs[test.i_type.rd] = gp_regs[test.i_type.rs1] >> (test.i_type.imm & 0b0000000111111);
+                                }
+                                break;
+                            case 0x20:
+                                fmt::print("SRAI instr\n");
+                                if (test.i_type.rd != 0) // Prevent write to x0 (AKA, hardwire 0)
+                                {
+                                    gp_regs[test.i_type.rd] = (int32_t)gp_regs[test.i_type.rs1] >> (test.i_type.imm & 0b0000000111111);
+                                }
+                                break;
+                        }
+                    case 0x2:
+                    case 0x3:
+                    default:
+                        unimplemented_instr(test);
+                        break;
+                }
+                break;
+            case 0b0000011: // Integer Load I-Type
+                break;
+            case 0b0100011: // Integer Store S-Type
+                break;
+            case 0b1100011: // Integer Branch B-Type
+                break;
+            case 0b1101111: // Integer JAL J-Type
+                break;
+            case 0b1100111: // Integer JALR I-Type
+                break;
+            case 0b0110111: // Integer LUI U-Type
+                break;
+            case 0b0010111: // Integer AUIPC U-Type
+                break;
+            case 0b1110011: // Integer ECALL/EBREAK I-Type
+                break;
+            default:
+                unimplemented_instr(test);
+                //pc_reg+=1;
+                break;
+        }
+    }
 
     return 0;
 }
