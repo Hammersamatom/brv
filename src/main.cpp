a6da6342e97ae73128354d860c5cebfc627da539
Joseph Samuel Pacheco-Corwin <hammersamatom@gmail.com>
2022-12-16 05:22:19 -0500

    IMM instructions
    Implemented IMM instructions
    They're gonna need extensive testing, since I'm certain I screwed up implementing the shift instructions... or basically anything that is sign-extended or zero-extended

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include <cstdint>
+#include <array>
 #include <fmt/core.h>
 
-
-union instr {
+union instr
+{
     uint32_t instruction;
-    struct __attribute__((__packed__)) op_only {
+    struct __attribute__((__packed__)) op_only
+    {
         uint8_t opcode : 7;
         uint32_t unused : 25;
     } op_only;
-    struct __attribute__((__packed__)) r_type {
+    struct __attribute__((__packed__)) r_type
+    {
         uint8_t opcode : 7;
         uint8_t rd : 5;
         uint8_t funct3 : 3;
@@ -16,14 +19,16 @@ union instr {
         uint8_t rs2 : 5;
         uint8_t funct7 : 7;
     } r_type;
-    struct __attribute__((__packed__)) i_type {
+    struct __attribute__((__packed__)) i_type
+    {
         uint8_t opcode : 7;
         uint8_t rd : 5;
         uint8_t funct3 : 3;
         uint8_t rs1 : 5;
         uint16_t imm : 12;
     } i_type;
-    struct __attribute__((__packed__)) s_type {
+    struct __attribute__((__packed__)) s_type
+    {
         uint8_t opcode : 7;
         uint8_t imm4_0 : 5;
         uint8_t funct3 : 3;
@@ -31,7 +36,8 @@ union instr {
         uint8_t rs2 : 5;
         uint8_t imm11_5 : 7;
     } s_type;
-    struct __attribute__((__packed__)) b_type {
+    struct __attribute__((__packed__)) b_type
+    {
         uint8_t opcode : 7;
         uint8_t imm11 : 1;
         uint8_t imm4_1 : 4;
@@ -41,12 +47,14 @@ union instr {
         uint8_t imm10_5 : 6;
         uint8_t imm12 : 1;
     } b_type;
-    struct __attribute__((__packed__)) u_type {
+    struct __attribute__((__packed__)) u_type
+    {
         uint8_t opcode : 7;
         uint8_t rd : 5;
         uint32_t imm31_12 : 20;
     } u_type;
-    struct __attribute__((__packed__)) j_type {
+    struct __attribute__((__packed__)) j_type
+    {
         uint8_t opcode : 7;
         uint8_t rd : 5;
         uint8_t imm19_12 : 8;
@@ -65,25 +73,24 @@ void unimplemented_instr(instr unimp)
     fmt::print("> Opcode: {:07b}\n", (uint8_t)unimp.op_only.opcode);
 }
 
-
 int main()
 {
     instr test;
 
-    uint32_t rom_code[] = {
+    std::array<uint32_t, 3> rom_code = {
         0x06408093, // ADDI x1, x1, 100
         0x0c810113, // ADDI x2, x2, 200
-        0x001101b3  // ADD  x3, x2, x1
+        0x001101b3 // ADD  x3, x2, x1
     };
 
     uint32_t gp_regs[32];
     uint32_t pc_reg = 0;
 
-    for (uint32_t i = 0; i < 1; i++)
+    for (uint32_t i = 0; i < rom_code.size(); i++)
     {
-
-        //test.instruction = (rom_code[pc_reg + 3] << 24) | (rom_code[pc_reg + 2] << 16) | (rom_code[pc_reg + 1] << 8) | (rom_code[pc_reg]);
-        test.instruction = rom_code[i];//pc_reg];
+        // test.instruction = (rom_code[pc_reg + 3] << 24) | (rom_code[pc_reg + 2]
+        // << 16) | (rom_code[pc_reg + 1] << 8) | (rom_code[pc_reg]);
+        test.instruction = rom_code[i]; // pc_reg];
         switch (test.op_only.opcode)
         {
             case 0b0110011: // Integer ALU R-Type
@@ -98,7 +105,6 @@ int main()
                                 {
                                     gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] + gp_regs[test.r_type.rs2];
                                 }
-                                //pc_reg+=1;
                                 break;
                             case 0x20:
                                 fmt::print("SUB instr\n");
@@ -106,11 +112,9 @@ int main()
                                 {
                                     gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] - gp_regs[test.r_type.rs2];
                                 }
-                                //pc_reg+=1;
                                 break;
                             default:
                                 unimplemented_instr(test);
-                                //pc_reg+=1;
                                 break;
                         }
                         break;
@@ -123,11 +127,9 @@ int main()
                                 {
                                     gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] ^ gp_regs[test.r_type.rs2];
                                 }
-                                //pc_reg+=1;
                                 break;
                             default:
                                 unimplemented_instr(test);
-                                //pc_reg+=1;
                                 break;
                         }
                         break;
@@ -140,11 +142,9 @@ int main()
                                 {
                                     gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] | gp_regs[test.r_type.rs2];
                                 }
-                                //pc_reg+=1;
                                 break;
                             default:
                                 unimplemented_instr(test);
-                                //pc_reg+=1;
                                 break;
                         }
                         break;
@@ -157,11 +157,9 @@ int main()
                                 {
                                     gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] & gp_regs[test.r_type.rs2];
                                 }
-                                //pc_reg+=1;
                                 break;
                             default:
                                 unimplemented_instr(test);
-                                //pc_reg+=1;
                                 break;
                         }
                         break;
@@ -174,11 +172,9 @@ int main()
                                 {
                                     gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] << gp_regs[test.r_type.rs2];
                                 }
-                                //pc_reg+=1;
                                 break;
                             default:
                                 unimplemented_instr(test);
-                                //pc_reg+=1;
                                 break;
                         }
                         break;
@@ -191,7 +187,6 @@ int main()
                                 {
                                     gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] >> gp_regs[test.r_type.rs2];
                                 }
-                                //pc_reg+=1;
                                 break;
                             case 0x20:
                                 fmt::print("SRA instr\n");
@@ -199,15 +194,15 @@ int main()
                                 {
                                     gp_regs[test.r_type.rd] = (int32_t)gp_regs[test.r_type.rs1] >> gp_regs[test.r_type.rs2];
                                 }
-                                //pc_reg+=1;
                                 break;
                             default:
                                 unimplemented_instr(test);
-                                //pc_reg+=1;
                                 break;
                         }
                         break;
-                    case 0x2: // SLT (Set Less Than [signed?]) (RV32I only has one instruction for funct3)
+                    case 0x2: // SLT (Set Less Than [signed?]) (RV32I only has one
+                        // instruction
+                        // for funct3)
                         switch (test.r_type.funct7)
                         {
                             case 0x00:
@@ -216,11 +211,9 @@ int main()
                                 {
                                     gp_regs[test.r_type.rd] = (signed)gp_regs[test.r_type.rs1] < (signed)gp_regs[test.r_type.rs2] ? 1 : 0;
                                 }
-                                //pc_reg+=1;
                                 break;
                             default:
                                 unimplemented_instr(test);
-                                //pc_reg+=1;
                                 break;
                         }
                         break;
@@ -233,11 +226,9 @@ int main()
                                 {
                                     gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] < gp_regs[test.r_type.rs2] ? 1 : 0;
                                 }
-                                //pc_reg+=1;
                                 break;
                             default:
                                 unimplemented_instr(test);
-                                //pc_reg+=1;
                                 break;
                         }
                         break;
@@ -246,7 +237,10 @@ int main()
                         break;
                 }
                 break;
-            case 0b0010011: // Integer ALU I-Type ////////// NOTE: Immediates are sign-extended (they're signed by DEFAULT, their MSB gets extended to the left [all ones to the left])
+            case 0b0010011: // Integer ALU I-Type -- NOTE: Immediates are
+                // sign-extended
+                // (they're signed by DEFAULT, their MSB gets extended to
+                // the left [all ones to the left])
                 switch (test.i_type.funct3)
                 {
                     case 0x0: // ADDI
@@ -277,14 +271,14 @@ int main()
                             gp_regs[test.i_type.rd] = gp_regs[test.i_type.rs1] & (test.i_type.imm >> 20);
                         }
                         break;
-                    case 0x1: // SLLI -- INCOMPLETE
+                    case 0x1: // SLLI -- NOTE: TEST
                         fmt::print("SLLI Instr\n");
                         if (test.i_type.rd != 0)
                         {
                             gp_regs[test.i_type.rd] = gp_regs[test.i_type.rs1] << ((test.i_type.imm >> 20) & 0b11111);
                         }
                         break;
-                    case 0x5:
+                    case 0x5: // SRLI / SRAI -- NOTE: TEST
                         switch ((test.i_type.imm & 0b111111100000) >> 5) // Subdivide the IMM value again with a union?
                         {
                             case 0x00:
@@ -301,9 +295,24 @@ int main()
                                     gp_regs[test.i_type.rd] = (int32_t)gp_regs[test.i_type.rs1] >> (test.i_type.imm & 0b0000000111111);
                                 }
                                 break;
+                            default:
+                                unimplemented_instr(test);
+                                break;
                         }
-                    case 0x2:
-                    case 0x3:
+                    case 0x2: // SLTI (Set Less Than Immediate) -- NOTE: TEST
+                        fmt::print("SLTI Instr\n");
+                        if (test.i_type.rd != 0)
+                        {
+                            gp_regs[test.i_type.rd] = (signed)gp_regs[test.i_type.rs1] < (signed)(test.i_type.imm >> 20) ? 1 : 0;
+                        }
+                        break;
+                    case 0x3: // SLTIU (Set Less Than Immediate Unsigned) -- NOTE: TEST
+                        fmt::print("SLTIU Instr\n");
+                        if (test.i_type.rd != 0)
+                        {
+                            gp_regs[test.i_type.rd] = gp_regs[test.i_type.rs1] < (test.i_type.imm >> 20) ? 1 : 0;
+                        }
+                        break;
                     default:
                         unimplemented_instr(test);
                         break;
@@ -327,10 +336,8 @@ int main()
                 break;
             default:
                 unimplemented_instr(test);
-                //pc_reg+=1;
                 break;
         }
     }
-
     return 0;
 }
