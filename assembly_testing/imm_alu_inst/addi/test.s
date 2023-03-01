707e27337e7f6412defea6e371802039cbd172fd
Hammersamatom <hammersamatom@gmail.com>
2023-03-01 02:27:32 -0500

    Added some assembly for testing
    Definitely not everything, and a testing infrastructure
    still needs to be set up, but it'll do for now

---
+++ assembly_testing/imm_alu_inst/addi/test.s
@@ -0,0 +1,35 @@
+.global _start
+_start:
+    addi x0, x0, 1000
+    addi x1, x0, 1000
+    addi x2, x0, 1000
+    addi x3, x0, 1000
+    addi x4, x0, 1000
+    addi x5, x0, 1000
+    addi x6, x0, 1000
+    addi x7, x0, 1000
+    addi x8, x0, 1000
+    addi x9, x0, 1000
+    addi x10, x0, 1000
+    addi x11, x0, 1000
+    addi x12, x0, 1000
+    addi x13, x0, 1000
+    addi x14, x0, 1000
+    addi x15, x0, 1000
+    addi x16, x0, 1000
+    addi x17, x0, 1000
+    addi x18, x0, 1000
+    addi x19, x0, 1000
+    addi x20, x0, 1000
+    addi x21, x0, 1000
+    addi x22, x0, 1000
+    addi x23, x0, 1000
+    addi x24, x0, 1000
+    addi x25, x0, 1000
+    addi x26, x0, 1000
+    addi x27, x0, 1000
+    addi x28, x0, 1000
+    addi x29, x0, 1000
+    addi x30, x0, 1000
+    addi x31, x0, 1000
+    ebreak
