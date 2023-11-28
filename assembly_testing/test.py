1b06fa84ab16a06af363d6caa19370fbed37c186
Hammersamatom <hammersamatom@gmail.com>
2023-11-28 11:02:01 -0500

    Add ANDI instruction + CXX generation unit and start work on python test script

--- assembly_testing/test.py
+++ assembly_testing/test.py
@@ -1,4 +1,6 @@
 import json as js;
+import sys;
+import subprocess;
 
 BITS = "32";
 ARCHITECTURE = "riscv" + BITS;
@@ -9,43 +11,45 @@ TEST_FILE_NAME = "test"; # The name of the assembly file to assemble and run
 RESULT_FILE_NAME = "expect_results"; # The name of the json file with the expect results, fail if they don't match
 LINKER_SCRIPT = "linker.ld";
 
-clang_build_command = "clang --target=" + ARCHITECTURE + " -march=" + SUBSET + " " + TEST_FILE_NAME + ".s -c -o obj.o -nostdlib"; # Compile to object file
-clang_link_command  = "ld.lld --script=" + LINKER_SCRIPT + "obj.o -o linked.bin"; # Link it
-clang_strip_command = "llvm-objcopy-10 -O binary linked.bin " + TEST_FILE_NAME + ".stripped"; # Strip out everything but the actual instructions
+#build_command = "clang --target=" + ARCHITECTURE + " -march=" + SUBSET + " " + TEST_FILE_NAME + ".s -c -o obj.o -nostdlib"; # Compile to object file
+#link_command  = "riscv32-elf-ld --script=" + LINKER_SCRIPT + "obj.o -o linked.bin"; # Link it
+#strip_command = "riscv32-elf-objcopy -O binary linked.bin " + TEST_FILE_NAME + ".stripped"; # Strip out everything but the actual instructions
 
-gcc_build_command = "riscv64-unknown-elf-gcc -march=" + SUBSET + " -mabi=" + ABI + " -g -ffreestanding -O0 -Wl,--gc-sections -nostartfiles -nostdlib -nodefaultlibs -Wl,-T," + LINKER_SCRIPT + " " + TEST_FILE_NAME + ".s"
-gcc_strip_command = "riscv64-unknown-elf-objcopy -O binary a.out " + TEST_FILE_NAME + ".stripped"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def main():
+    num_args = len(sys.argv);
+    if num_args < 2:
+        print("Not enough arguemnts!");
+        print("Feed the path to a test")
+    build_output = subprocess.run(
+        [
+            "clang",
+            "--target=" + ARCHITECTURE,
+            "-march=" + SUBSET,
+            sys.argv[1] + TEST_FILE_NAME + ".s",
+            "-c",
+            "-o",
+            sys.argv[1] + "obj.o",
+            "-nostdlib"
+        ]
+    );
+    linker_output = subprocess.run(
+        [
+            "riscv32-elf-ld",
+            "--script=" + sys.argv[1] + LINKER_SCRIPT,
+            sys.argv[1] + "obj.o",
+            "-o",
+            sys.argv[1] + "linked.bin"
+        ]
+    );
+    strip_output = subprocess.run(
+        [
+            "riscv32-elf-objcopy",
+            "-O",
+            "binary",
+            sys.argv[1] + "linked.bin",
+            sys.argv[1] + TEST_FILE_NAME + ".stripped"
+        ]
+    );
 
 
 if __name__ == "__main__":
