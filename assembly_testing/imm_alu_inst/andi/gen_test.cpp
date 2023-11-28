cc4e9aabad4112c546029bf2af59c5f07f456990
Hammersamatom <hammersamatom@gmail.com>
2023-11-28 11:02:01 -0500

    Add ANDI instruction + CXX generation unit and start work on python test script

---
+++ assembly_testing/imm_alu_inst/andi/gen_test.cpp
@@ -0,0 +1,31 @@
+#include <fmt/core.h>
+#include <time.h>
+
+int main ()
+{
+    srand(time(NULL));
+
+    int a_res[32] = {0};
+    int b_res[32] = {0};
+    int and_results[32] = {0};
+
+    for (int i = 1; i < 32; i++)
+    {
+        a_res[i] = (rand() % 4096) - 2048;
+        b_res[i] = (rand() % 4096) - 2048;
+        and_results[i] = a_res[i] & b_res[i];
+    }
+
+    for (int j = 0; j < 32; j++)
+    {
+        fmt::print("addi x{0}, x0, {1}\n", j, a_res[j]);
+    }
+    for (int j = 0; j < 32; j++)
+    {
+        fmt::print("andi x{0}, x{0}, {1}\n", j, b_res[j]);
+    }
+
+    
+    for (int j = 0; j < 32; j++)
+        fmt::print("{}\n", and_results[j]);
+}
