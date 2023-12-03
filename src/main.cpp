05723d25970221125588d1e9853e68436b7a7661
Hammersamatom <hammersamatom@gmail.com>
2023-12-03 16:01:49 -0500

    Improved condensing of code
    Reg->Imm fallthrough
    Load and Store fallthrough
    Consolidation of the branch instructions
    Replaced x0-31 register array with a class that automatically rejects
    attempts to modify x0, which improved compiled instruction counts for
    ESP32, x86 (Clang and GCC), and RISC-V targets.

--- src/main.cpp
+++ src/main.cpp
@@ -57,6 +57,27 @@ inline int32_t sign_extend(uint32_t a, uint8_t shift)
     return (int32_t)(a << shift) >> shift;
 }
 
+template <typename T>
+class registerFile
+{
+    private:
+        T registers[32] = { 0 };
+    public: 
+        T* data() {
+            return registers;
+        }
+    public:
+        void setReg(uint8_t acc, T in)
+        {
+            if (acc) registers[acc & 0x1F] = in;
+        }
+
+        const T& operator[](uint8_t acc)
+        {
+            return registers[acc & 0x1F];
+        }
+};
+
 int main(int argc, char* argv[])
 {
     fmt::print("Got {} arguments\n", argc);
@@ -68,8 +89,12 @@ int main(int argc, char* argv[])
     std::unique_ptr<uint8_t[]> memory = std::make_unique<uint8_t[]>(MEM_MAX);
     std::memset(memory.get(), 0, MEM_MAX * sizeof(*memory.get()));
 
-    component _ebreak; _ebreak.word = 0x00100073;
-    for(std::size_t m = 0; m < 4; m++) memory[m] = _ebreak.byte[m];
+    //uint8_t memory[MEM_MAX] = { 0 };
+
+    //component _ebreak; _ebreak.word = 0x00100073;
+    //for(std::size_t m = 0; m < 4; m++) memory[m] = _ebreak.byte[m];
+    memory[0] = 0x73;
+    memory[2] = 0x10;
 
     std::ifstream binary;
     if (argc > 1)
@@ -102,7 +127,8 @@ int main(int argc, char* argv[])
 
     // BEGIN INTERPRETATION
     instr test;
-    uint32_t gp_regs[32] = { 0 };
+    //uint32_t gp_regs[32] = { 0 };
+    registerFile<uint32_t> gp_regs;
     uint32_t pc_reg = 0;
 
     for (;;)
@@ -114,7 +140,10 @@ int main(int argc, char* argv[])
         helper.byte[3] = memory[pc_reg + 3];
         test.instruction = helper.word;
 
-        bool branched = false;
+        uint8_t branched = 0;
+
+        bool rs2_or_imm = helper.word & 0x20;
+        uint32_t val = rs2_or_imm ? gp_regs[test.r_type.rs2] : sign_extend(test.i_type.imm, 20);
 
         imm_reconstruct imm; imm.word = 0;
         component t; t.word = 0;
@@ -122,119 +151,63 @@ int main(int argc, char* argv[])
 
         switch (test.op_only.opcode)
         {
-            case 0b0110011: // Integer ALU R-Type
-                switch (test.r_type.funct3)
-                {
-                    case 0x0: // ADD / SUB
-                        switch (test.r_type.funct7)
-                        {
-                            case 0x00: gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] + gp_regs[test.r_type.rs2]; break;
-                            case 0x20: gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] - gp_regs[test.r_type.rs2]; break;
-                        }
-                        break;
-                    case 0x4: // XOR (RV32I only has one instruction for funct3)
-                        switch (test.r_type.funct7)
-                        {
-                            case 0x00: gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] ^ gp_regs[test.r_type.rs2]; break;
-                        }
-                        break;
-                    case 0x6: // OR (RV32I only has one instruction for funct3)
-                        switch (test.r_type.funct7)
-                        {
-                            case 0x00: gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] | gp_regs[test.r_type.rs2]; break;
-                        }
-                        break;
-                    case 0x7: // AND (RV32I only has one instruction for funct3)
-                        switch (test.r_type.funct7)
-                        {
-                            case 0x00: gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] & gp_regs[test.r_type.rs2]; break;
-                        }
-                        break;
-                    case 0x1: // SLL (RV32I only has one instruction for funct3)
-                        switch (test.r_type.funct7)
-                        {
-                            case 0x00: gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] << gp_regs[test.r_type.rs2]; break;
-                        }
-                        break;
-                    case 0x5: // SRL / SRA (Should be working fine)
-                        switch (test.r_type.funct7)
-                        {
-                            case 0x00: gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] >> gp_regs[test.r_type.rs2]; break;
-                            case 0x20: gp_regs[test.r_type.rd] = (int32_t)gp_regs[test.r_type.rs1] >> gp_regs[test.r_type.rs2]; break;
-                        }
-                        break;
-                    case 0x2: // SLT (Set Less Than [signed?]) (RV32I only has one instruction for funct3)
-                        switch (test.r_type.funct7)
-                        {
-                            case 0x00: gp_regs[test.r_type.rd] = (int32_t)gp_regs[test.r_type.rs1] < (int32_t)gp_regs[test.r_type.rs2] ? 1 : 0; break;
-                        }
-                        break;
-                    case 0x3: // SLTU (Set Less Than Unsigned) (RV32I only has one instruction for funct3)
-                        switch (test.r_type.funct7)
-                        {
-                            case 0x00: gp_regs[test.r_type.rd] = gp_regs[test.r_type.rs1] < gp_regs[test.r_type.rs2] ? 1 : 0; break;
-                        }
-                        break;
-                }
-                break;
+            case 0b0110011: // Integer ALU R-Type - Fallthrough
             case 0b0010011: // Integer ALU I-Type NOTE: Immediates are sign-extended (they're signed by DEFAULT, their MSB gets extended to the left [all ones to the left])
                 switch (test.i_type.funct3)
                 {
                     // ADDI -- TESTED
-                    case 0x0: gp_regs[test.i_type.rd] = gp_regs[test.i_type.rs1] + sign_extend(test.i_type.imm, 20); break;
+                    case 0x0: 
+                        switch (test.r_type.funct7)
+                        {
+                            case 0x00: gp_regs.setReg(test.r_type.rd, gp_regs[test.i_type.rs1] + val); break;
+                            case 0x20: gp_regs.setReg(test.r_type.rd, gp_regs[test.i_type.rs1] - gp_regs[test.r_type.rs2]); break;
+                        }
                     // XORI -- TESTED
-                    case 0x4: gp_regs[test.i_type.rd] = gp_regs[test.i_type.rs1] ^ sign_extend(test.i_type.imm, 20); break;
+                    case 0x4: gp_regs.setReg(test.r_type.rd, gp_regs[test.i_type.rs1] ^ val); break;
                     // ORI
-                    case 0x6: gp_regs[test.i_type.rd] = gp_regs[test.i_type.rs1] | sign_extend(test.i_type.imm, 20); break;
+                    case 0x6: gp_regs.setReg(test.r_type.rd, gp_regs[test.i_type.rs1] | val); break;
                     // ANDI
-                    case 0x7: gp_regs[test.i_type.rd] = gp_regs[test.i_type.rs1] & sign_extend(test.i_type.imm, 20); break;
+                    case 0x7: gp_regs.setReg(test.r_type.rd, gp_regs[test.i_type.rs1] & val); break;
                     // SLLI / first 5 bits
-                    case 0x1: gp_regs[test.i_type.rd] = gp_regs[test.i_type.rs1] << (sign_extend(test.i_type.imm, 20) & 0x1F); break;
+                    case 0x1: gp_regs.setReg(test.r_type.rd, gp_regs[test.i_type.rs1] << (rs2_or_imm ? val : val & 0x1F)); break;
                     // SRLI / SRAI
                     case 0x5:
-                        switch ((test.i_type.imm & 0b111111100000) >> 5) // Subdivide the IMM value again with a union?
+                        switch ((test.i_type.imm & 0xFE0) >> 5) // Subdivide the IMM value again with a union?
                         {
-                            case 0x00: gp_regs[test.i_type.rd] = gp_regs[test.i_type.rs1] >> (sign_extend(test.i_type.imm, 20) & 0x1F); break;
-                            case 0x20: gp_regs[test.i_type.rd] = (int32_t)gp_regs[test.i_type.rs1] >> (sign_extend(test.i_type.imm, 20) & 0x1F); break;
+                            case 0x00: gp_regs.setReg(test.r_type.rd, gp_regs[test.i_type.rs1] >> (rs2_or_imm ? val : val & 0x1F)); break;
+                            case 0x20: gp_regs.setReg(test.r_type.rd, (int32_t)gp_regs[test.i_type.rs1] >> (rs2_or_imm ? val : val & 0x1F)); break;
                         }
                         break;
                     // SLTI (Set Less Than Immediate)
-                    case 0x2: gp_regs[test.i_type.rd] = (int32_t)gp_regs[test.i_type.rs1] < (int32_t)sign_extend(test.i_type.imm, 20) ? 1 : 0; break;
+                    case 0x2: gp_regs.setReg(test.r_type.rd, (int32_t)gp_regs[test.i_type.rs1] < (signed)val ? 1 : 0); break;
                     // SLTIU (Set Less Than Immediate Unsigned)
-                    case 0x3: gp_regs[test.i_type.rd] = gp_regs[test.i_type.rs1] < sign_extend(test.i_type.imm, 20 ? 1 : 0); break;
+                    case 0x3: gp_regs.setReg(test.r_type.rd, gp_regs[test.i_type.rs1] < val ? 1 : 0); break;
                 }
                 break;
             case 0b0000011: // Integer Load I-Type -- ~~TESTED~~ / Needs retesting, switched to a UNION
                 ls_offset = gp_regs[test.i_type.rs1] + sign_extend(test.i_type.imm, 20);
                 switch (test.i_type.funct3)
                 {
+
+                    case 0x1: // LH (Load Half, sign-extended)
+                        // Need the lowest 16-bits, sign extended, immediate value
+                        t.byte[3] = memory[ls_offset + 1];
+                        t.byte[2] = memory[ls_offset + 0];
+                        gp_regs.setReg(test.r_type.rd, t.word_s >> 16);
+                        break;
                     case 0x0: // LB (Load Byte, sign extended)
                         // Need the lowest 8-bits, sign extended, immediate value
                         t.byte[3] = memory[ls_offset + 0];
-                        gp_regs[test.i_type.rd] = t.word_s >> 24;
-                        break;
-                    case 0x1: // LH (Load Half, sign-extended)
-                        // Need the lowest 16-bits, sign extended, immediate value
-                        t.byte[2] = memory[ls_offset + 0];
-                        t.byte[3] = memory[ls_offset + 1];
-                        gp_regs[test.i_type.rd] = t.word_s >> 16;
+                        gp_regs.setReg(test.r_type.rd, t.word_s >> 24);
                         break;
                     case 0x2: // LW (Load Word)
-                        t.byte[0] = memory[ls_offset + 0];
-                        t.byte[1] = memory[ls_offset + 1];
-                        t.byte[2] = memory[ls_offset + 2];
                         t.byte[3] = memory[ls_offset + 3];
-                        gp_regs[test.i_type.rd] = t.word;
-                        break;
+                        t.byte[2] = memory[ls_offset + 2];
+                    case 0x5: // LHU (Load Half Unsigned)
+                        t.byte[1] = memory[ls_offset + 1];
                     case 0x4: // LBU (Load Byte Unsigned)
                         t.byte[0] = memory[ls_offset + 0];
-                        gp_regs[test.i_type.rd] = t.word;
-                        break;
-                    case 0x5: // LHU (Load Half Unsigned)
-                        t.byte[0] = memory[ls_offset + 0];
-                        t.byte[1] = memory[ls_offset + 1];
-                        gp_regs[test.i_type.rd] = t.word;
-                        break;
+                        gp_regs.setReg(test.r_type.rd, t.word);
                 }
                 break;
             case 0b0100011: // Integer Store S-Type
@@ -243,73 +216,38 @@ int main(int argc, char* argv[])
                 ls_offset = gp_regs[test.s_type.rs1] + sign_extend(imm.word, 20);
                 switch (test.s_type.funct3)
                 {
+                    case 0x2: //SW
+                        memory[gp_regs[test.s_type.rs1] + ls_offset + 3] = t.byte[3];
+                        memory[gp_regs[test.s_type.rs1] + ls_offset + 2] = t.byte[2];
+                    case 0x1: // SH
+                        memory[gp_regs[test.s_type.rs1] + ls_offset + 1] = t.byte[1];
                     case 0x0: // SB
                         memory[gp_regs[test.s_type.rs1] + ls_offset + 0] = t.byte[0];
-                        break;
-                    case 0x1: // SH
-                        memory[gp_regs[test.s_type.rs1] + ls_offset + 0] = t.byte[0];
-                        memory[gp_regs[test.s_type.rs1] + ls_offset + 1] = t.byte[1];
-                        break;
-                    case 0x2: //SW
-                        memory[gp_regs[test.s_type.rs1] + ls_offset + 0] = t.byte[0];
-                        memory[gp_regs[test.s_type.rs1] + ls_offset + 1] = t.byte[1];
-                        memory[gp_regs[test.s_type.rs1] + ls_offset + 2] = t.byte[2];
-                        memory[gp_regs[test.s_type.rs1] + ls_offset + 3] = t.byte[3];
-                        break;
                 }
                 break;
             case 0b1100011: // Integer Branch B-Type
                 imm.b_imm = {0, test.b_type.imm4_1, test.b_type.imm10_5, test.b_type.imm11, test.b_type.imm12, 0};
                 switch (test.b_type.funct3)
                 {
-                    case 0x0: // BEQ -- Signed or unsigned doesn't matter, since either it's equal or not.
-                        if (gp_regs[test.b_type.rs1] == gp_regs[test.b_type.rs2])
-                        {
-                            branched = true;
-                            pc_reg += sign_extend(imm.word, 19);
-                        }
-                        break;
-                    case 0x1: // BNE -- Signed or unsigned doesn't matter, since either it's equal or not.
-                        if (gp_regs[test.b_type.rs1] != gp_regs[test.b_type.rs2])
-                        {
-                            branched = true;
-                            pc_reg += sign_extend(imm.word, 19);
-                        }
-                        break;
-                    case 0x4: // BLT
-                        if ((signed)gp_regs[test.b_type.rs1] < (signed)gp_regs[test.b_type.rs2])
-                        {
-                            branched = true;
-                            pc_reg += sign_extend(imm.word, 19);
-                        }
-                        break;
-                    case 0x5: // BGE
-                        if ((signed)gp_regs[test.b_type.rs1] >= (signed)gp_regs[test.b_type.rs2])
-                        {
-                            branched = true;
-                            pc_reg += sign_extend(imm.word, 19);
-                        }
-                        break;
-                    case 0x6: // BLTU
-                        if (gp_regs[test.b_type.rs1] < gp_regs[test.b_type.rs2])
-                        {
-                            branched = true;
-                            pc_reg += sign_extend(imm.word, 19);
-                        }
-                        break;
-                    case 0x7: // BGEU
-                        if (gp_regs[test.b_type.rs1] >= gp_regs[test.b_type.rs2])
-                        {
-                            branched = true;
-                            pc_reg += sign_extend(imm.word, 19);
-                        }
-                        break;
+                    // BEQ -- Signed or unsigned doesn't matter, since either it's equal or not.
+                    case 0x0: if (gp_regs[test.b_type.rs1] == gp_regs[test.b_type.rs2]) branched = 1; break;
+                    // BNE -- Signed or unsigned doesn't matter, since either it's equal or not.
+                    case 0x1: if (gp_regs[test.b_type.rs1] != gp_regs[test.b_type.rs2]) branched = 1; break;
+                    // BLT
+                    case 0x4: if ((signed)gp_regs[test.b_type.rs1] < (signed)gp_regs[test.b_type.rs2]) branched = 1; break;
+                    // BGE
+                    case 0x5: if ((signed)gp_regs[test.b_type.rs1] >= (signed)gp_regs[test.b_type.rs2]) branched = 1; break;
+                    // BLTU
+                    case 0x6: if (gp_regs[test.b_type.rs1] < gp_regs[test.b_type.rs2]) branched = 1; break;
+                    // BGEU
+                    case 0x7: if (gp_regs[test.b_type.rs1] >= gp_regs[test.b_type.rs2]) branched = 1; break;
                 }
+                if (branched) pc_reg += sign_extend(imm.word, 19);
                 break;
             case 0b1101111: // Integer JAL J-Type
                 {
                     imm.j_imm = {0, test.j_type.imm10_1, test.j_type.imm11, test.j_type.imm19_12, test.j_type.imm20, 0};
-                    gp_regs[test.j_type.rd] = pc_reg + 4;
+                    gp_regs.setReg(test.j_type.rd, pc_reg + 4);
                     branched = true;
                     pc_reg += sign_extend(imm.word, 11);
                 }
@@ -320,17 +258,18 @@ int main(int argc, char* argv[])
                 {
                     case 0x0:
                         {
-                            gp_regs[test.i_type.rd] = pc_reg + 4;
+                            gp_regs.setReg(test.r_type.rd, pc_reg + 4);
                             branched = true;
-                            pc_reg = gp_regs[test.i_type.rs1] + sign_extend(test.i_type.imm, 20);
+                            //pc_reg = gp_regs[test.i_type.rs1] + sign_extend(test.i_type.imm, 20);
+                            pc_reg = gp_regs[test.i_type.rs1] + ((int32_t)test.instruction >> 20);
                         }
                         break;
                 }
                 break;
             // Integer LUI U-Type
-            case 0b0110111: if (test.u_type.rd != 0) gp_regs[test.u_type.rd] = (uint32_t)test.u_type.imm31_12 << 12; break;
+            case 0b0110111: gp_regs.setReg(test.u_type.rd, (uint32_t)test.u_type.imm31_12 << 12); break;
             // Integer AUIPC U-Type
-            case 0b0010111: if (test.u_type.rd != 0) gp_regs[test.u_type.rd] = pc_reg + ((uint32_t)test.u_type.imm31_12 << 12); break;
+            case 0b0010111: gp_regs.setReg(test.u_type.rd, pc_reg + ((uint32_t)test.u_type.imm31_12 << 12)); break;
             case 0b1110011: // Integer ECALL/EBREAK I-Type
                 switch (test.i_type.funct3)
                 {
@@ -340,7 +279,7 @@ int main(int argc, char* argv[])
                             // ECALL
                             case 0x0: break;
                             // EBREAK
-                            case 0x1: spit_registers(gp_regs, pc_reg); fmt::print("{}\n", spit_registers_json(gp_regs, pc_reg)); return 0; break;
+                            case 0x1: spit_registers(gp_regs.data(), pc_reg); return 0; break;
                         }
                         break;
                 }
@@ -348,7 +287,7 @@ int main(int argc, char* argv[])
         }
 
         // Reset zero/x0 register to 0. Prevent branching.
-        gp_regs[0] = 0;
+        //gp_regs[0] = 0;
 
         // Increment PC if we haven't branched/jumped
         if (!branched)
