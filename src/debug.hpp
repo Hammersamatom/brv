56c22adc7463554e6327f77639aab95ff90e8f23
Hammersamatom <hammersamatom@gmail.com>
2024-03-28 03:06:17 -0400

    Added include guard to debug.hpp
    In case it's used in more files than main.cpp

--- src/debug.hpp
+++ src/debug.hpp
@@ -1,6 +1,11 @@
+#ifndef DEBUG_HPP
+#define DEBUG_HPP
+
 #include <string>
 #include "unions.hpp"
 
 void unimplemented_instr(instr unimp, uint32_t* regs, uint32_t pc);
 void spit_registers(uint32_t* regs, uint32_t pc);
 std::string spit_registers_json(const uint32_t* regs, const uint32_t& pc);
+
+#endif
